b1681c2195c37dbadfc4a696757f8cf905b2c77d
Mike Helgeson <mike.helgeson@aura.com>
2024-09-12 14:36:53 -0400

    Add bass accompaniment for karma police

--- src/songs.js
+++ src/songs.js
@@ -8,7 +8,8 @@ const songs = {
     "C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2 G#3C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2",
   _7nation: "E3/4. E3/8 G3/6 E3/6 D3/6 | C3/2 B2/2 | E3/4. E3/8 G3/6 E3/6 D3/6 | C3/6 D3/6 C3/6 B2/2",
   furelise: "E4/8 D#4/8 | E4/8 D#4/8 E4/8 B3/8 Db4/8 C4/8 | A3/4 R/8 C3/8 E3/8 A3/8",
-  karma: "R/8 C4/4 D4/4 C4/4 B3/8 | B3/4 R/8. B4/16 E3/8 B4/4 C3/8 | C3/8 A3/4 D4/4 C3/4 B4/8 | B4/4 R/8 R/16 A3/16 E4/16 D4/16 C4/8 B4/8 C4/8"
+  karmaR: "R/8 C5/4 D5/4 C5/4 B4/8 | B4/4 R/8. B4/16 E5/8 B4/4 C5/8 | C5/8 A4/4 D5/4 C5/4 B4/8 | B4/4 R/8 R/16 A4/16 E5/16 D5/16 C5/8 B4/8 C5/8",
+  karmaL: "A2/4 C4A3E3/4 D2/8 D2/8 C4A3F3/4 | D2/4 B3G#3E3/8 F2/8 G2/4 B3G#3D3/4 | A2/4 C4A3E3/4 C4A3G3F3/4 F4C4A3F3/4 | E2/4 B3G#3E3/8 F2/8 G2/4 B3G#3D3/4",
   // wonderful world?
   // We Will Rock You
   // clint eastwood
