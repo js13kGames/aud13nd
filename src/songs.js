22a511433e6e4fef9edd5064781282a7f1e94969
Mike Helgeson <mike.helgeson@aura.com>
2024-09-12 10:33:29 -0400

    more songs

--- src/songs.js
+++ src/songs.js
@@ -6,9 +6,10 @@ const songs = {
     "E4/16 E4/8 E4/16 R/16 C4/16 E4/8 G4/4 G3/4 | C4/8 R/16 G3/16 R/8 E3/8 R/16 A3/16 R/16 B3/16 R/16 Bb3/16 A3/8 | G3/8 E4/8 G4/8 A4/8 F4/16 G4/16 R/16 E4/16 R/16 C4/16 D4/16 B3/16 R/8",
   chords:
     "C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2 G#3C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2",
-  // für elise
+  _7nation: "E3/4. E3/8 G3/6 E3/6 D3/6 | C3/2 B2/2 | E3/4. E3/8 G3/6 E3/6 D3/6 | C3/6 D3/6 C3/6 B2/2",
+  furelise: "E4/8 D#4/8 | E4/8 D#4/8 E4/8 B3/8 Db4/8 C4/8 | A3/4 R/8 C3/8 E3/8 A3/8",
+  karma: "R/8 C4/4 D4/4 C4/4 B3/8 | B3/4 R/8. B4/16 E3/8 B4/4 C3/8 | C3/8 A3/4 D4/4 C3/4 B4/8 | B4/4 R/8 R/16 A3/16 E4/16 D4/16 C4/8 B4/8 C4/8"
   // wonderful world?
-  // seven nation army
   // We Will Rock You
   // clint eastwood
   // instant crush
