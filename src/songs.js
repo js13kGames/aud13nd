2e80a42cdf7b7899eef5d98a9c892be1b930e12f
Mike Helgeson <mike.helgeson@aura.com>
2024-09-12 20:20:47 -0400

    cleanup songs

--- src/songs.js
+++ src/songs.js
@@ -1,15 +1,39 @@
 const songs = {
-  cmajor:
-    "C3/4 D3/4 E3/4 F3/4 G3/4 A3/4 B3/4 C4/4 B3/4 A3/4 G3/4 F3/4 E3/4 D3/4 C3/4",
+  cmajor: "C3/4 D3/4 E3/4 F3/4 G3/4 A3/4 B3/4 C4/4",
   twinkle: "C3/4 C3/4 G3/4 G3/4 A3/4 A3/4 G3/2",
-  mario:
-    "E4/16 E4/8 E4/16 R/16 C4/16 E4/8 G4/4 G3/4 | C4/8 R/16 G3/16 R/8 E3/8 R/16 A3/16 R/16 B3/16 R/16 Bb3/16 A3/8 | G3/8 E4/8 G4/8 A4/8 F4/16 G4/16 R/16 E4/16 R/16 C4/16 D4/16 B3/16 R/8",
-  chords:
-    "C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2 G#3C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2",
-  _7nation: "E3/4. E3/8 G3/6 E3/6 D3/6 | C3/2 B2/2 | E3/4. E3/8 G3/6 E3/6 D3/6 | C3/6 D3/6 C3/6 B2/2",
-  furelise: "E4/8 D#4/8 | E4/8 D#4/8 E4/8 B3/8 Db4/8 C4/8 | A3/4 R/8 C3/8 E3/8 A3/8",
-  karmaR: "R/8 C5/4 D5/4 C5/4 B4/8 | B4/4 R/8. B4/16 E5/8 B4/4 C5/8 | C5/8 A4/4 D5/4 C5/4 B4/8 | B4/4 R/8 R/16 A4/16 E5/16 D5/16 C5/8 B4/8 C5/8",
-  karmaL: "A2/4 C4A3E3/4 D2/8 D2/8 C4A3F3/4 | D2/4 B3G#3E3/8 F2/8 G2/4 B3G#3D3/4 | A2/4 C4A3E3/4 C4A3G3F3/4 F4C4A3F3/4 | E2/4 B3G#3E3/8 F2/8 G2/4 B3G#3D3/4",
+  mario: `
+    E4/16 E4/8 E4/16 R/16 C4/16 E4/8 G4/4 G3/4
+    C4/8 R/16 G3/16 R/8 E3/8 R/16 A3/16 R/16 B3/16 R/16 Bb3/16 A3/8
+    G3/8 E4/8 G4/8 A4/8 F4/16 G4/16 R/16 E4/16 R/16 C4/16 D4/16 B3/16 R/8
+  `,
+  chords1: "Ab4C5Eb5 Bb4Db5F5 Eb4G4Bb4Db5 F4Ab4C5",
+  chords2: `
+    C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2
+    G#3C#3A#2F#2/2 G#3F#3C#3/2 F#3C#3B2/2 G#3F#3C#3/2
+  `,
+  seven_nation: `
+    E3/4. E3/8 G3/6 E3/6 D3/6
+    C3/2 B2/2
+    E3/4. E3/8 G3/6 E3/6 D3/6
+    C3/6 D3/6 C3/6 B2/2
+  `,
+  fur_elise: `
+    E4/8 D#4/8
+    E4/8 D#4/8 E4/8 B3/8 Db4/8 C4/8
+    A3/4 R/8 C3/8 E3/8 A3/8
+  `,
+  karma_lead: `
+    R/8 C5/4 D5/4 C5/4 B4*3/8
+    R/8. B4/16 E5/8 B4/4 C5/4
+    A4/4 D5/4 C5/4 B4*3/8
+    R/8 R/16 A4/16 E5/16 D5/16 C5/8 B4/8 C5/8
+  `,
+  karma_acc: `
+    A2/4 C4A3E3/4 D2/8 D2/8 C4A3F3/4
+    D2/4 B3G#3E3/8 F2/8 G2/4 B3G#3D3/4
+    A2/4 C4A3E3/4 C4A3G3F3/4 F4C4A3F3/4
+    E2/4 B3G#3E3/8 F2/8 G2/4 B3G#3D3/4
+  `,
   // wonderful world?
   // We Will Rock You
   // clint eastwood
